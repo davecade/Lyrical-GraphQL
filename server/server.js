7c0e3796b8feba9ce494cadc57c785a7928cb213
Dave <dave.cadelina@outlook.com>
2021-10-06 01:57:37 +1100

    setup mongodb and connected it to our server

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://dbDavec:EzidwlpBINubhADn@lyricaldb.r1k42.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
