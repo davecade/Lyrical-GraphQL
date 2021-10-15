fd941afdc4ee1d5192a3fb82bd3f16b51e0c6a5c
Dave <dave.cadelina@outlook.com>
2021-10-16 01:17:52 +1100

    added routes to navigate

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
+import { Link } from 'react-router'
 
 const SongList = ({data: { songs }}) => {
 
     return (
-        <ul className="collection">
-            {
-                songs ?
-                songs.map((song, index) => <li className="collection-item" key={index}>{song.title}</li>)
-                :
-                <div>{"Loading songs..."}</div>}
-        </ul>
+        <div>
+            <ul className="collection">
+                {
+                    songs ?
+                    songs.map((song, index) => <li className="collection-item" key={index}>{song.title}</li>)
+                    :
+                    <div>{"Loading songs..."}</div>
+                }
+            </ul>
+            <Link to="/songs/new" className="btn-floating btn-large red right">
+                <i className="material-icons">add</i>
+            </Link>
+        </div>
     )
 }
 
