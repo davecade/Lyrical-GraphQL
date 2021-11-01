889b43cfa6a90758a9435f50dd45f151ef9752c7
Dave <dave.cadelina@outlook.com>
2021-11-02 00:53:32 +1100

    accessed params in props for the URL id

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { graphql } from 'react-apollo'
+import fetchSong from '../queries/fetchSong'
 
-const SongDetail = () => {
+const SongDetail = (props) => {
+    console.log(props)
     return (
         <div>
             <h3>Song Detail</h3>
