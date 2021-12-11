da620c20a8623bd2a2343933866053efa4c10ed7
Dave <dave.cadelina@outlook.com>
2021-12-11 11:42:08 +1100

    started adding thumbs up

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
 
 class LyricList extends Component {
+    onLike(id) {
+        console.log("ID", id)
+    }
+
     renderLyrics() {
         return this.props.lyrics.map(({ id, content }) => (
             <li key={id} className="collection-item">
                 {content}
+                <i
+                    className="material-icons"
+                    style={{
+                        marginLeft: "100%",
+                        marginRight: "1rem",
+                        cursor: "pointer",
+                    }}
+                    onClick={() => this.onLike(id)}
+                >
+                    thumb_up
+                </i>
             </li>
         ));
     }
